46b15eae88c3efd7539b1a81944257f6733a4d80
Bao Tran <Baottran21@outlook.com>
2024-02-12 16:46:08 -0600

    Working through router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import NavBar from "./components/Navigation/Nav";
+// import NavBar from "./components/Navigation/Nav";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <h1 className="text-[12rem]">This is a test</h1>
+      {/* <NavBar /> */}
+      <AppRouter />
     </>
   );
 }
