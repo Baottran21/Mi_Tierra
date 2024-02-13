148bbdc0036db7b8b155ab275f7f4a37f812a3ff
Bao Tran <Baottran21@outlook.com>
2024-02-13 17:10:00 -0600

    added menu to menu page

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,10 +1,28 @@
+import MenuPage1 from "./../../assets/MI TIERRA PAGE 1.png";
+import MenuPage2 from "./../../assets/MI TIERRA PAGE 2.png";
+import MenuPage3 from "./../../assets/MI TIERRA PAGE 3.png";
+import MenuPage4 from "./../../assets/MI TIERRA PAGE 4.png";
 const Menu = () => {
   return (
     <>
       <div
         id="Menu-page-container"
-        className=" w-full h-[80vh] flex justify-evenly items-center"
+        className=" w-full  flex justify-evenly items-center"
       >
+        <ul>
+          <li>
+            <img src={MenuPage1} alt="Menu Page 1"></img>
+          </li>
+          <li>
+            <img src={MenuPage2} alt="Menu Page 2"></img>
+          </li>
+          <li>
+            <img src={MenuPage3} alt="Menu Page 3"></img>
+          </li>
+          <li>
+            <img src={MenuPage4} alt="Menu Page 4"></img>
+          </li>
+        </ul>
       </div>
     </>
   );
