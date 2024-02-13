f9cfb4a7f032d3fd0c2be19570857278ed28b3b0
Bao Tran <Baottran21@outlook.com>
2024-02-13 16:36:52 -0600

    Working on href to menu

--- src/Components/LandingPage/LandingPage.jsx
+++ src/Components/LandingPage/LandingPage.jsx
@@ -5,7 +5,15 @@ const LandingPage = () => {
         id="landing-page-container"
         className=" w-full h-[80vh] flex justify-evenly items-center"
       >
-        <h1>This is the Landing Page</h1>
+        <div className="w-full flex-row justify-center items-center">
+          <h1 className="flex justify-center">
+            We Apologize for the inconvenience but we are currently working
+            through website maintenance
+          </h1>
+          <a href="/menu">
+            <h1>Menu</h1>
+          </a>
+        </div>
       </div>
     </>
   );
